4ebe4011d31d2aee363af5663198624b53fefeee
at1208 <amantiwari12081999@gmail.com>
2024-08-18 18:04:49 +0530

    fix: changes

---
+++ config.py
@@ -0,0 +1,7 @@
+
+# config/config.py
+URL = "https://visa.vfsglobal.com/tha/en/ltp/login"
+EMAIL = "aman.geeksocean@gmail.com"
+PASSWORD = "Aman@2024"
+API_ENDPOINT="http://localhost:8080/api/visas/VISA_003"
+API_TOKEN="khsb231kbc211232_weqdcxds"
